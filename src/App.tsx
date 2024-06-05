aee28f2ff18a32ff702f4735adb062244979e863
BYCODDE <anaraliev217@gmail.com>
2024-06-05 15:00:51 +0400

    api ending logic is done working on input value and onsumbmit!

--- src/App.tsx
+++ src/App.tsx
@@ -3,51 +3,52 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 
-  export type dataTypes = {
-    name: string;
-    login: string;
-    created_at: string;
-    bio: string;
-    public_repos: number;
-    followers: number;
-    following: number;
-    avatar_url: string;
-    html_url: string;
-    id: number;
-    email: string;
-    blog: string;
-    location: string;
-    twitter_username: string;
-    company: string;
-    following_url: string;
-    followers_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
-    darkMode: boolean;
-  };
+export type dataTypes = {
+  name: string;
+  login: string;
+  created_at: string;
+  bio: string;
+  public_repos: number;
+  followers: number;
+  following: number;
+  avatar_url: string;
+  html_url: string;
+  id: number;
+  email: string;
+  blog: string;
+  location: string;
+  twitter_username: string;
+  company: string;
+  following_url: string;
+  followers_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  darkMode: boolean;
+};
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
   const [data, setData] = useState<dataTypes | null>(null);
+  const [Username, setUsername] = useState<string>("BYCODDE");
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   };
-  async function fechData() {
-    const response = await fetch("https://api.github.com/users/octocat");
+  async function fetchData() {
+    const response = await fetch(`https://api.github.com/users/${Username}`);
     const result = await response.json();
     setData(result);
     console.log(data);
   }
 
   useEffect(() => {
-    fechData();
+    fetchData();
   }, []);
 
   return (
